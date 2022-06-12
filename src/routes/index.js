e5cefab54c9a76504dbe6da63b604f2169c384c3
SaidLoop <saidfernandolgon@ufps.edu.co>
2022-06-12 02:50:55 -0500

    fancy

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 //Raiz
-router.get("/index", async (req, res) => {
+router.get("/", async (req, res) => {
     res.render("links/index");
   });
 
